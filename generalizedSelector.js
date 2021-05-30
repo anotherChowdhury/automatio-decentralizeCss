11066f73eee04fbe84ecbbf87c9385adf00fcf29
anotherChowdhury <umarfaruk.chowdhury@gmail.com>
2021-05-31 01:34:56 +0600

    second approach

--- generalizedSelector.js
+++ generalizedSelector.js
@@ -1,54 +1,150 @@
 /*
+decentralizeCss(`h2.heading--2eONR.heading-2--1OnX8.title--3yncE.block--3v-Ow`)
+// h2[class*="heading--"][class*="heading-2--"][class*="title--"][class*="block--"]
 
-"h2.heading--2eONR.heading-2--1OnX8.title--3yncE.block--3v-Ow"
+decentralizeCss(`[class="actions--3vB_X nextButton--25Tal gtm-next-page"] div`)
+// [class*="actions--"][class*="nextButton--"][class*="gtm-next-page"] div
 
-`h2[class*="heading--"][class*="heading-2--"][class*="title--"][class*="block--"]`
+decentralizeCss(`.css-ntxj49 [class="css-8buua1-Typeahead"]`)
+// [class*="css-"] [class*="css-"][class*="-Typeahead"]
+
+decentralizeCss(`div.css-18eqh53-Input > input#downshift-0-input`)
+// div[class*="css-"][class*="-Input"] > input[id^="downshift-"][id$="-input"]
+
+decentralizeCss(`[id="downshift-0-input"]`)
+// input[id^="downshift-"][id$="-input"]
 
 */
 
 const symbolToWords = {
-  '.': 'class',
-  '#': 'id'
+  class: '*',
+  id: '^'
 }
 
 const htmlTagSet = new Set(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'div', 'ul', 'li', 'body', 'input', 'button'])
 
 function isRandom(str) {
+  if (!str) return true
   // it is a random string if two consecutive string is a mix of number,uppercase,lowercase
   let test = false
-  for (i = 0; i < str.length - 1; i++) {
+  for (let i = 0; i < str.length - 1; i++) {
     const curr = str[i] + str[i + 1]
-    test = /\d[a-z|A-Z]|[a-z|A-Z]\d|[a-z][A-Z]|[A-Z][a-z]/.test(curr)
+    test = /\d[a-z|A-Z]|[a-z|A-Z]\d/.test(curr)
     if (test) break
   }
   return test
 }
 
+// const decentralizeCss = (cssPath) => {
+//   let generalizedCss = ''
+//   let idx = cssPath.length - 1
+//   let currentWord = ''
+//   while (idx >= 0) {
+//     console.log(currentWord)
+//     const currentCharacter = cssPath[idx]
+//     if (!symbolToWords[currentCharacter]) {
+//       if (['-',']',"="].includes(currentCharacter)) {
+//         if (isRandom(currentWord)) currentWord = ''
+//       }
+//       currentWord = currentCharacter + currentWord
+//     } else {
+//       generalizedCss = `[${symbolToWords[currentCharacter]}*="${currentWord}"]` + generalizedCss
+//       currentWord = ''
+//     }
+
+//     if (htmlTagSet.has(currentWord)) {
+//       generalizedCss = currentWord + generalizedCss
+//       currentWord = ''
+//     }
+//     idx--
+//   }
+
+//   return generalizedCss
+// }
+
+// second approach
+
+// [class="actions--3vB_X nextButton--25Tal gtm-next-page"] div
+
 const decentralizeCss = (cssPath) => {
-  let generalizedCss = ''
-  let idx = cssPath.length - 1
-  let currentWord = ''
-  while (idx >= 0) {
-    console.log(currentWord)
-    const currentCharacter = cssPath[idx]
-    if (!symbolToWords[currentCharacter]) {
-      if (currentCharacter == '-') {
-        if (isRandom(currentWord)) currentWord = ''
-      }
-      currentWord = currentCharacter + currentWord
-    } else {
-      generalizedCss = `[${symbolToWords[currentCharacter]}*="${currentWord}"]` + generalizedCss
-      currentWord = ''
-    }
+  let genericCss = ''
+  let cssPathList = ''
+  const classSelectionExists = cssPath.indexOf(']')
+  if (classSelectionExists > -1) {
+    const opening = cssPath.indexOf('[')
+    opening < classSelectionExists ? (cssPathList = cssPath.split('[')) : (cssPathList = cssPath.split(']'))
+    console.log(cssPathList)
+  } else cssPathList = cssPath.split(' ')
 
-    if (htmlTagSet.has(currentWord)) {
-      generalizedCss = currentWord + generalizedCss
-      currentWord = ''
-    }
-    idx--
+  for (const path of cssPathList) {
+    const a = getGeneralizedCss(path)
+    console.log(a)
+    genericCss += a
   }
-
-  return generalizedCss
+  console.log(genericCss)
+  return genericCss
 }
 
-console.log(decentralizeCss('h2.heading--2eONR.heading-2--1OnX8.title--3yncE.block--3v-Ow'))
+const getGeneralizedCss = (str, tag = '') => {
+  let genericCss = ''
+  //[class="actions--3vB_X nextButton--25Tal gtm-next-page"]
+  if (str[0] == '[') return handleGeneralizedSelector(str)
+  else if (str.indexOf('.') > -1) return handleClassSelector(str)
+  else if (str.indexOf('#') > -1) return handleIdSelector(str)
+  else {
+    const nameList = str.split('-')
+    let currentAttributeName = ''
+    for (let idx = 0; idx < nameList.length; idx++) {
+      const name = nameList[idx].replace('"').trim()
+
+      if (htmlTagSet.has(name)) {
+        console.log(name)
+        if (!genericCss) genericCss += ' ' + name
+        else genericCss += name
+      } else if (!isRandom(name)) {
+        if (idx + 1 <= nameList.length - 1) {
+          if (!nameList[idx + 1]) currentAttributeName += `${name}--`
+          else currentAttributeName += `${name}-`
+        } else {
+          if (!currentAttributeName) currentAttributeName = '-' + name
+          else currentAttributeName += name
+        }
+      } else {
+        if (currentAttributeName) genericCss += `[${tag}${symbolToWords[tag]}="${currentAttributeName}"]`
+        currentAttributeName = ''
+      }
+    }
+    if (currentAttributeName) genericCss += `[${tag}${symbolToWords[tag]}="${currentAttributeName}"]`
+    return genericCss
+  }
+}
+
+const handleGeneralizedSelector = (str) => {
+  str = str.substring(1, str.length)
+  let genericCss = ''
+  const pathList = str.split('=')
+  const tag = pathList[0]
+  const selectorList = pathList[pathList.length - 1].split(' ')
+  selectorList.forEach((element) => {
+    element = element.replace('"', '')
+    genericCss += getGeneralizedCss(element, tag)
+  })
+  console.log(genericCss)
+  return genericCss
+}
+
+const handleClassSelector = (str) => {
+  let genericCss = ''
+  const selectorList = str.split('.')
+  selectorList.forEach((element) => (genericCss += getGeneralizedCss(element, 'class')))
+  return genericCss
+}
+
+const handleIdSelector = (str) => {
+  let genericCss = ''
+  const selectorList = str.split('#')
+  selectorList.forEach((element, idx) => (genericCss += getGeneralizedCss(element, 'id')))
+  return genericCss
+}
+
+decentralizeCss('.css-ntxj49 [class="css-8buua1-Typeahead"]')
